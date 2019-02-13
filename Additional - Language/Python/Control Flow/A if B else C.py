43636c748abb67b4b831da2f2536a525a773bf56
JUD210_Hyuk <judicious210@gmail.com>
2019-02-13 09:27:32 +0900

    {Py} [HR - RegEx] P: 30/47

--- Additional - Language/Python/Control Flow/A if B else C.py
+++ Additional - Language/Python/Control Flow/A if B else C.py
@@ -1,5 +1,5 @@
 for num in range(-3, 4):
-    print("{n:2} >= 0".format(n=num)) if num >= 0 else print(str(num) + " < 0")
+    print("{n:2} >= 0".format(n=num)) if num >= 0 else print(str(num) + " < 0")  # type: ignore
 # -3 < 0
 # -2 < 0
 # -1 < 0
