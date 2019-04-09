cb255201f0c6fa3c6e375791435aa488f45ebc99
JUD210_Hyuk <judicious210@gmail.com>
2019-04-09 20:15:49 +0900

    {Py} [HR - RegEx] P: 32/47

---
+++ Training/RegEx - HackerRank/7. Applications/Detect the Email Addresses.py
@@ -0,0 +1,44 @@
+# https://www.hackerrank.com/challenges/detect-the-email-addresses/problem
+
+
+import re
+
+# Inputs
+standard_input = """14
+Letters to the Editor (Your complete mailing address is required):
+letters@thehindu.co.in
+Readers' Editor:
+readerseditor@thehindu.co.in
+Advertisements Queries (Print):
+inetads@thehindu.co.in
+Advertisements Queries (Online):
+digital@thehindu.co.in
+Advertisements Queries (International):
+international@thehindu.co.in
+Subscription Queries:
+subs@thehindu.co.in
+Comments on the website:
+web.thehindu@thehindu.co.in"""
+
+
+emails = []
+for _ in range(int(input())):
+
+    line = input().strip()
+
+    email_pattern = r"\w+(?:\.\w+)*@\w+(?:\.\w+)*"
+
+    for found in re.findall(email_pattern, line):
+        emails.append(found)
+
+
+emails = sorted(set(emails), key=str)
+
+
+result = ""
+for email in emails:
+    result += f"{email};"
+
+print(result[:-1])
+
+# digital@thehindu.co.in;inetads@thehindu.co.in;international@thehindu.co.in;letters@thehindu.co.in;readerseditor@thehindu.co.in;subs@thehindu.co.in;web.thehindu@thehindu.co.in
